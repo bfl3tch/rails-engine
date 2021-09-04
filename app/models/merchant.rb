ac0fbace9f170af3cb026e267eefdbe199a6cf62
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 19:50:00 -0400

    Abstract method to model where it BELONGS!

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
-
   validates :name, presence: true
+
+  def self.fetch_requested_merchants(per_page, page)
+    Merchant.all.limit(per_page).offset(page)
+  end
 end
