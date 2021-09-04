01f3915e9067922b29ff1da82be1edf8ebf538dd
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 02:21:06 -0400

    Configure routes IAW namespace guidelines

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, module: :merchants, only: [:index]
+      end
+      resources :items do
+        resources :merchants, only: [:index]
+      end
+    end
+  end
 end
