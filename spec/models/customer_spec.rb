3f67e22d6962434bef7bd36d264982414366191d
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 02:20:45 -0400

    Create model association and validation tests for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'associations' do
-    expect
+    it { should have_many(:invoices) }
+    it { should have_many(:merchants).through(:invoices) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name)}
+    it { should validate_presence_of(:last_name)}
   end
 end
