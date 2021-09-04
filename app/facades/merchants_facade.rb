ebcb5fbe75d54ef66e06477fd3e76abe1f43d94b
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 19:50:39 -0400

    Create merchants facade to handle functional coding for controller

---
+++ app/facades/merchants_facade.rb
@@ -0,0 +1,13 @@
+class MerchantsFacade
+  def self.per_page(params)
+    params.nil? ? 20 : params.to_i
+  end
+
+  def self.page(params)
+    (params.nil? || params.to_i <= 0) ? 0 : (params.to_i - 1) * 20
+  end
+  
+  def self.fetch_requested_merchants(per_page, page)
+    Merchant.fetch_requested_merchants(per_page, page)
+  end
+end
