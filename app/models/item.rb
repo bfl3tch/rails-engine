85e11861c64cd5c327e2a21cea2c4e62522b26b5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 02:21:37 -0400

    Create model associations and validates to pass tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true, numericality: true
 end
