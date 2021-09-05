1778a441fb8d2e8d54b42e6dd1be2b490200535f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 22:02:47 -0400

    Create model method to pass spec test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,8 @@ class Merchant < ApplicationRecord
   def self.fetch_requested_merchants(per_page, page)
     Merchant.all.limit(per_page).offset(page)
   end
+
+  def self.fetch_merchant_items(merchant)
+    Merchant.find_by_id(merchant.id).items if merchant
+  end
 end
