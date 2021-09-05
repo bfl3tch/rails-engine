36d9ada8af5a8075f06fbdaed1efe74eecf9d696
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-04 22:03:21 -0400

    Setup rescue recordnotfound rescue response to pass postman tests

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,19 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
-
   included do
-    # rescue_from ActiveRecord::RecordNotFound do |e|
-    #   json_response({ message: e.message }, :not_found)
-    # end
 
-    # rescue_from ActiveRecord::RecordInvalid do |e|
-    #   json_response({ message: e.message }, :unprocessable_entity)
-    # end
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json:
+        {
+          errors:
+          [
+            {
+              status: e.status,
+              message: e.message,
+              code: e.code
+            }
+          ]
+        }
+    end
   end
 end
