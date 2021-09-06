22b51249aac8b5ffff5bb19072cebba8e73b5409
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-06 12:44:45 -0400

    Update merchants controller to render error items instead of AR::error

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -9,13 +9,32 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def show
-    json_response(MerchantSerializer.new(@merchant), status)
-    raise ActiveRecord::RecordNotFound if @merchant.nil?
+    json_response(MerchantSerializer.new(@merchant), status) if @merchant
+    json_response(ErrorMerchantSerializer.new(@error_merchant), :not_found) if @merchant.nil?
   end
+
+    # json_response(ErrorMerchantSerializer.new(@error_merchant), status) if @merchant.nil?
+
+
+    # json_response({message: 'Merchant with that ID not found'}, :not_found) if @merchant.nil?
+
+    # raise ActiveRecord::RecordNotFound if @merchant.nil?
+
+     # rescue ActiveRecord::RecordNotFound
+    # begin
+      # @merchant.nil?
+    # rescue ActiveRecord::RecordNotFound
+      # render json:
+      # {
+      #   message: 'Not Found',
+      #   errors: ["Could not find merchant with id of #{params[:id]}"]
+      # }, status: :not_found if @merchant.nil?
+    # end
 
   private
 
   def set_merchant
     @merchant = Merchant.find_by_id(params[:id])
+    @error_merchant = ErrorMerchant.new("No merchant found with that ID") if @merchant.nil?
   end
 end
