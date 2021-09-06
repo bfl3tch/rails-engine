3e50463965314c297d3a43ac2335c3f737d88828
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-06 12:44:23 -0400

    Commented out exception handler module methods for now

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,19 +1,28 @@
 module ExceptionHandler
+  extend Response
   extend ActiveSupport::Concern
   included do
 
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render json:
-        {
-          errors:
-          [
-            {
-              status: e.status,
-              message: e.message,
-              code: e.code
-            }
-          ]
-        }
-    end
+    # rescue_from ActiveRecord::RecordNotFound do |e|
+    #   render json:
+    #     {
+    #       errors:
+    #       [
+    #         {
+    #           status: e.status,
+    #           message: e.message,
+    #           code: e.code
+    #         }
+    #       ]
+    #     }
+    # end
+
+    # rescue_from ActiveRecord::RecordNotFound do |e|
+    #   json_response({ message: e.message }, :not_found)
+    #   end
+    #
+    # rescue_from ActiveRecord::RecordInvalid do |e|
+    #   json_response({ message: e.message }, :unprocessable_entity)
+    # end
   end
 end
