2ddccdcccbd74228c10d226c29a45a0d56f2ad82
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-06 12:43:29 -0400

    Create model test for item for getting all and specific item/s

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
+
+  def self.fetch_requested_items(per_page, page)
+    Item.all.limit(per_page).offset(page)
+  end
+
+  def self.fetch_item(item)
+    Item.find_by_id(item.id) if item
+  end
 end
