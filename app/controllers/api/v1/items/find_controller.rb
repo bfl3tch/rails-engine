e9a407ff0fb69cc7c10924fd4d01886adec8c22a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-08 15:01:42 -0400

    Create find controller with actions

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,18 +1,28 @@
 class Api::V1::Items::FindController < ApplicationController
-  # before_action :set_item, only: [:index]
+  before_action :set_item, only: [:index]
 
   def index
-    require "pry"; binding.pry
-    @search_results = ItemsFacade.item_search(params[:name])
-    # json_response(MerchantSerializer.new(@merchant), status) if @item && @merchant
-    # json_response(ErrorItemSerializer.new(@error_item), :not_found) if @item.nil?
+    json_response(ItemSerializer.new(@item), status) if @item
+    json_response(ErrorSerializer.new(@error_item), :bad_request) if @error_item && @item.nil?
+    json_response(UnfoundItemSerializer.new(@bad_item), :bad_request) if @item.nil? && @error_item.nil?
   end
 
   private
 
   def set_item
-    # @item = Item.find_by_id(params[:item_id])
-    # @merchant = Merchant.find_by_id(@item.merchant_id) if @item
-    # @error_item = ErrorItem.new("No item found with that ID") if @item.nil?
+    name = params[:name] if params[:name]
+    min = params[:min_price].to_i if params[:min_price]
+    max = params[:max_price].to_i if params[:max_price]
+
+    @bad_item = ErrorItem.new("No items in that price range") if (min && min > 10000)
+    @error_item = ErrorItem.new("Price Below Zero") if (max && max < 0)
+    @error_item = ErrorItem.new("Price Below Zero") if (min && min < 0)
+    @error_item = ErrorItem.new("Unable to process request") if name && (min.present? || max.present?)
+    @error_item = ErrorItem.new("No match for that name") if (name && ItemsFacade.item_search(name).nil?)
+
+    @item = ItemsFacade.item_search(name) if name && (min.nil? && max.nil?)
+    @item = ItemsFacade.both_price_search(min, max) if (min && max) && name.nil? && ((min >= 0) && (max >= 0))
+    @item = ItemsFacade.min_price_search(min) if min && (name.nil? && max.nil? && (min >= 0))
+    @item = ItemsFacade.max_price_search(max) if max && (name.nil? && min.nil? && (max >= 0))
   end
 end
