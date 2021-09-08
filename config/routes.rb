2ab0805fc4ebaefeba7c2b8eae4ded518cc18b47
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-07 21:06:23 -0400

    Tried using namespace and resources, ended up handrolling routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
 
+  # get '/api/v1/items/find', to: '/api/v1/items/find#index'
+
   namespace :api do
    namespace :v1 do
-
-     resources :items, except: [:new, :edit] do
-       resources :merchant, module: :items, only: [:index]
-     end
-
+     get '/items/find', to: 'items/find#index'
+     get '/items/find_all', to: 'items/find#show'
      resources :merchants, only: [:index, :show] do
        resources :items, module: :merchants, only: [:index]
      end
-  # namespace :api do
-  #   namespace :v1 do
-  #
-  #     resources :merchants, only: [:index, :show] do
-  #       resources :items, module: :merchants, only: [:index]
-  #     end
-  #
-  #     resources :items do
-  #       resources :merchants, module: :items, only: [:index]
-  #     end
+     resources :items, except: [:new, :edit] do
+       resources :merchant, module: :items, only: [:index]
+     end
+     # namespace :items do
+     #   resources :find_all, module: :items, only: [:index]
+     #   resources :find, module: :items, only: [:index]
+     # end
+     # namespace :merchants do
+     #   resources :find_all, module: :merchants, only: [:index]
+     #   resources :find, module: :merchants, only: [:index]
+     # end
     end
   end
 end
