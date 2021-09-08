754a615d797468cdbfb944c1b3d65c62745ce9ff
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-08 15:02:38 -0400

    Adjust poro for output

--- app/poros/error_item.rb
+++ app/poros/error_item.rb
@@ -1,9 +1,8 @@
 class ErrorItem
-  attr_reader :error_message
+  attr_reader :error
 
-  def initialize(error_message)
-    # require "pry"; binding.pry
-    @id = 1
-    @error_message = error_message
+  def initialize(error)
+    # @id = 1
+    @error = error
   end
 end
