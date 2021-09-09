0b0f5057b2e8d13d0aa98e6b236ed8e15e1ba071
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-09 17:55:34 -0400

    Create model spec test for revenue for one and a set of merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,6 +12,50 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of(:name) }
   end
 
+  describe 'instance methods' do
+    describe '#merchant_revenue' do
+      it 'returns the revenue for one merchant' do
+        merchant_1 = create(:merchant)
+
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+        customer_3 = create(:customer)
+
+        item_1 = create(:item, merchant: merchant_1)
+        item_2 = create(:item, merchant: merchant_1)
+
+        invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
+
+        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 15, unit_price: 1)
+        invoice_item_2 = create(:invoice_item, item: item_2, invoice: invoice_1, quantity: 20, unit_price: 2)
+
+        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
+
+        expect(merchant_1.merchant_revenue).to eq(55)
+      end
+
+      it 'returns the revenue for one merchant with floats' do
+        merchant_1 = create(:merchant)
+
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+        customer_3 = create(:customer)
+
+        item_1 = create(:item, merchant: merchant_1)
+        item_2 = create(:item, merchant: merchant_1)
+
+        invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
+
+        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 15, unit_price: 123.56)
+        invoice_item_2 = create(:invoice_item, item: item_2, invoice: invoice_1, quantity: 20, unit_price: 88.79)
+
+        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
+
+        expect(merchant_1.merchant_revenue).to eq(3629.2000000000003)
+      end
+    end
+  end
+
   describe 'class methods' do
     describe '::fetch_requested_merchants' do
       it 'returns merchants with the requested parameters appropriately' do
@@ -69,48 +113,6 @@ RSpec.describe Merchant, type: :model do
       end
     end
 
-    describe '::merchant_revenue' do
-      it 'returns the revenue for one merchant' do
-        merchant_1 = create(:merchant)
-
-        customer_1 = create(:customer)
-        customer_2 = create(:customer)
-        customer_3 = create(:customer)
-
-        item_1 = create(:item, merchant: merchant_1)
-        item_2 = create(:item, merchant: merchant_1)
-
-        invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
-
-        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 15, unit_price: 1)
-        invoice_item_2 = create(:invoice_item, item: item_2, invoice: invoice_1, quantity: 20, unit_price: 2)
-
-        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
-
-        expect(Merchant.merchant_revenue(merchant_1.id).first.revenue).to eq(55)
-      end
-
-      it 'returns the revenue for one merchant with floats' do
-        merchant_1 = create(:merchant)
-
-        customer_1 = create(:customer)
-        customer_2 = create(:customer)
-        customer_3 = create(:customer)
-
-        item_1 = create(:item, merchant: merchant_1)
-        item_2 = create(:item, merchant: merchant_1)
-
-        invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
-
-        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 15, unit_price: 123.56)
-        invoice_item_2 = create(:invoice_item, item: item_2, invoice: invoice_1, quantity: 20, unit_price: 88.79)
-
-        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
-
-        expect(Merchant.merchant_revenue(merchant_1.id).first.revenue).to eq(3629.2000000000003)
-      end
-    end
-
     describe '::most_revenue' do
       it 'returns the merchants ordered by revenue descending' do
         merchant_1 = create(:merchant)
@@ -137,7 +139,39 @@ RSpec.describe Merchant, type: :model do
         transaction_2 = create(:transaction, invoice: invoice_2, result: 'success')
         transaction_3 = create(:transaction, invoice: invoice_3, result: 'success')
 
-        expect(Merchant.most_revenue).to eq([merchant_2, merchant_3, merchant_1])
+        expect(Merchant.most_revenue(3)).to eq([merchant_2, merchant_3, merchant_1])
+        expect(Merchant.most_revenue(2)).to eq([merchant_2, merchant_3])
+        expect(Merchant.most_revenue(1)).to eq([merchant_2])
+      end
+
+      it 'returns the merchants ordered by revenue descending with floats' do
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_3 = create(:merchant)
+
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+        customer_3 = create(:customer)
+
+        item_1 = create(:item, merchant: merchant_1)
+        item_2 = create(:item, merchant: merchant_2)
+        item_3 = create(:item, merchant: merchant_3)
+
+        invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1, status: 'shipped')
+        invoice_2 = create(:invoice, merchant: merchant_2, customer: customer_2, status: 'shipped')
+        invoice_3 = create(:invoice, merchant: merchant_3, customer: customer_3, status: 'shipped')
+
+        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 12, unit_price: 1.564)
+        invoice_item_2 = create(:invoice_item, item: item_2, invoice: invoice_2, quantity: 5, unit_price: 2.65415)
+        invoice_item_3 = create(:invoice_item, item: item_3, invoice: invoice_3, quantity: 10, unit_price: 3.2164)
+
+        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
+        transaction_2 = create(:transaction, invoice: invoice_2, result: 'success')
+        transaction_3 = create(:transaction, invoice: invoice_3, result: 'success')
+
+        expect(Merchant.most_revenue(3)).to eq([merchant_3, merchant_1, merchant_2])
+        expect(Merchant.most_revenue(2)).to eq([merchant_3, merchant_1])
+        expect(Merchant.most_revenue(1)).to eq([merchant_3])
       end
     end
   end
