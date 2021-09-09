e11dccaf93c5cb4d069adc73cc4072d942eec1d2
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-08 20:51:04 -0400

    Clean up controller with tertiary operators

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,24 +11,19 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    json_response(ItemSerializer.new(@item), status) if @item
-    json_response(ErrorItemSerializer.new(@error_item), :not_found) if @item.nil?
+    @item ? json_response(ItemSerializer.new(@item)) : json_response(ErrorItemSerializer.new(@error_item), :not_found)
   end
 
   def create
-    json_response(ItemSerializer.new(@new_item), :created) if @new_item.save
-    json_response(ErrorItemSerializer.new(@error_item), :bad_request) if !@new_item.save
+    @new_item.save ? json_response(ItemSerializer.new(@new_item), :created) : json_response(ErrorItemSerializer.new(@error_item), :bad_request)
   end
 
   def update
     @error_item ? json_response(ErrorItemSerializer.new(@error_item), :not_found) : json_response(ItemSerializer.new(@item), :created)
-    # json_response(ItemSerializer.new(@item), :created) :  if @item
-    # json_response(ErrorItemSerializer.new(@error_item), :not_found)
   end
 
   def destroy
-    json_response(ItemSerializer.new(@item.delete), :ok) if @item
-    json_response(ErrorItemSerializer.new(@error_item), :not_found) if @item.nil?
+    @item ? json_response(ItemSerializer.new(@item.delete)) : json_response(ErrorItemSerializer.new(@error_item), :not_found)
   end
 
   private
@@ -45,21 +40,14 @@ class Api::V1::ItemsController < ApplicationController
 
   def update_item
     @found_merchant = Merchant.find_by_id(item_params['merchant_id']) if item_params['merchant_id']
-
     if (@found_merchant || item_params['merchant_id'].nil?)
       @item.update(item_params) if @item
     elsif (@found_merchant.nil? && !item_params['merchant_id'].nil?)
       @error_item = ErrorItem.new("Merchant ID must match an existing Merchant") if @item
-    else
-      @error_item = ErrorItem.new("No item found with that ID")
     end
   end
 
   def item_params
     params.require(:item).permit(:id, :name, :description, :unit_price, :merchant_id)
-  end
-
-  def update_item_params
-    params.require(:item).permit(:id, :name, :description, :unit_price)
   end
 end
