ab75b9e3923fceb25435d90b446a4c69442fbe00
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-09 18:57:57 -0400

    Create facade method to abstract model request from controller

--- app/facades/merchants_facade.rb
+++ app/facades/merchants_facade.rb
@@ -14,4 +14,9 @@ class MerchantsFacade
   def self.search_by_name(search)
     Merchant.find_by_name(search)
   end
+
+  def self.most_revenue_ranked(search)
+    Merchant.most_revenue(search)
+  end
+
 end
