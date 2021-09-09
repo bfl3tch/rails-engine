01aa64a518f4a9195be751e5cf68e29d051a3d44
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-09 18:05:43 -0400

    Polish serializer

--- app/serializers/merchant_revenue_serializer.rb
+++ app/serializers/merchant_revenue_serializer.rb
@@ -2,6 +2,5 @@ class MerchantRevenueSerializer
   include JSONAPI::Serializer
   attributes :revenue do |merchant|
     merchant.merchant_revenue
-  # attribute :revenue, &:merchant_revenue
   end
 end
