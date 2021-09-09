04e73e96717368e149c1d189e9d89789c93983c5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-08 22:05:37 -0400

    Craete model spec test for searching for merchant by name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -41,5 +41,29 @@ RSpec.describe Merchant, type: :model do
         expect(Merchant.fetch_merchant_items(merchant2)).not_to eq([item1, item3])
       end
     end
+
+    describe '::find_by_name' do
+      it 'returns all merchants with matches to that name' do
+        merchant1 = create(:merchant, name: 'Joe')
+        merchant2 = create(:merchant, name: 'joe')
+        merchant3 = create(:merchant, name: 'Joel')
+        merchant4 = create(:merchant, name: 'joelneville')
+        merchant5 = create(:merchant, name: 'bob')
+
+        expect(Merchant.find_by_name('joe')).to eq([merchant1, merchant3, merchant2, merchant4])
+      end
+
+      it 'doesnt return items from another merchant' do
+        merchant1 = create(:merchant)
+        merchant2 = create(:merchant)
+
+        item1 = create(:item, merchant: merchant1)
+        item2 = create(:item, merchant: merchant2)
+        item3 = create(:item, merchant: merchant1)
+
+        expect(Merchant.fetch_merchant_items(merchant1)).not_to eq([item2])
+        expect(Merchant.fetch_merchant_items(merchant2)).not_to eq([item1, item3])
+      end
+    end
   end
 end
