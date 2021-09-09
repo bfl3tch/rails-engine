a8635e6449c7654a2d69a1aa6232a0150fe2539c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-08 22:06:15 -0400

    Request specs for find_all merchant searches happy and sad

--- spec/requests/api/v1/items/find_request_spec.rb
+++ spec/requests/api/v1/items/find_request_spec.rb
@@ -136,4 +136,52 @@ RSpec.describe "Item Find API" do
       expect(response.status).to eq(400)
     end
   end
+
+  describe 'show request' do
+    it 'returns all merchants if they have partial matches to the search case insensitive' do
+      merchant1 = create(:merchant, name: 'Joe')
+      merchant2 = create(:merchant, name: 'joe')
+      merchant3 = create(:merchant, name: 'Joel')
+      merchant4 = create(:merchant, name: 'joelneville')
+      merchant5 = create(:merchant, name: 'bob')
+
+      get '/api/v1/merchants/find_all?name=joe'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response.body).to include(merchant1.name)
+      expect(response.body).to include(merchant2.name)
+      expect(response.body).to include(merchant3.name)
+      expect(response.body).to include(merchant4.name)
+      expect(response.body).to_not include(merchant5.name)
+    end
+
+    it 'returns empty dataset if no params are given' do
+      merchant1 = create(:merchant, name: 'Joe')
+      merchant2 = create(:merchant, name: 'joe')
+      merchant3 = create(:merchant, name: 'Joel')
+      merchant4 = create(:merchant, name: 'joelneville')
+      merchant5 = create(:merchant, name: 'bob')
+
+      get '/api/v1/merchants/find_all'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(204)
+      expect(response.body).to eq("")
+    end
+
+    it 'returns empty dataset array if unmatching params are given' do
+      merchant1 = create(:merchant, name: 'Joe')
+      merchant2 = create(:merchant, name: 'joe')
+      merchant3 = create(:merchant, name: 'Joel')
+      merchant4 = create(:merchant, name: 'joelneville')
+      merchant5 = create(:merchant, name: 'bob')
+
+      get '/api/v1/merchants/find_all?name=marinatedsteak'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("{\"data\":[]}")
+    end
+  end
 end
