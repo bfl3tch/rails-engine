00faff98b55d2939b3190b8efe90f00fa7ec4980
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-09 10:10:33 -0400

    Update associations on merchant, create method to determine revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,10 @@
 class Merchant < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
+  has_many :invoice_items, through: :items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
+  
   validates :name, presence: true
 
   def self.fetch_requested_merchants(per_page, page)
@@ -12,5 +17,18 @@ class Merchant < ApplicationRecord
 
   def self.find_by_name(search)
     where("lower(name) ILIKE ?", "%#{search.downcase}%").order(:name)
+  end
+
+  def self.merchant_revenue(merchant_id)
+    self.joins(:transactions)
+    .joins(:invoice_items)
+    .joins(:invoices)
+    .select("merchants.id as id, merchants.name as name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .where("merchants.id = ?", "#{merchant_id}")
+    .group(:name, :id)
+  end
+
+  def self.most_revenue
+
   end
 end
